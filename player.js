d31261d8cc0b80a2bcde2251c06019fb66bc6178
Denis Nelubin <dnelubin@gmail.com>
2015-08-01 12:23:12 +0600

    Refactored.

--- player.js
+++ player.js
@@ -2,42 +2,10 @@
 module.exports = {
 
   VERSION: "Crazy Bot",
-  RANK_SCORE: {
-    '2': 1,
-    '3': 2,
-    "4": 3,
-    "5": 4,
-    "6": 5,
-    "7": 6,
-    "8": 7,
-    "9": 8,
-    "10": 9,
-    "J": 10,
-    "Q": 11,
-    "K": 12,
-    "A": 13
-  },
 
   bet_request: function(game_state) {
     return bet_plus_blind(game_state, 2);
-  },
-
-  bet_strategy_by_score: function(game_state) {
-    try {
-      players = game_state.players;
-      sevenbits_bot = players.filter(function (player) {
-        return player.name == 'sevenbits';
-      })[0];
-      our_cards = sevenbits_bot.hole_cards;
-      score = RANK_SCORE[our_cards[0]['rank']] * RANK_SCORE[our_cards[1]['rank']];
-      if (score > 50) {
-        return 100000;
-      } else {
-        return 0;
-      }
-    } catch (e) {
-      return 0;
-    }
+    //return bet_strategy_by_score(game_state);
   },
 
   showdown: function(game_state) {
@@ -51,4 +19,38 @@ function bet_plus_blind(game_state, blinds) {
     maxBet = Math.max(p.bet, maxBet);
   });
   return maxBet + game_state.small_blind * blinds - maxBet % game_state.small_blind;
+}
+
+RANK_SCORE = {
+  '2': 1,
+      '3': 2,
+      "4": 3,
+      "5": 4,
+      "6": 5,
+      "7": 6,
+      "8": 7,
+      "9": 8,
+      "10": 9,
+      "J": 10,
+      "Q": 11,
+      "K": 12,
+      "A": 13
+};
+
+function bet_strategy_by_score(game_state) {
+  try {
+    players = game_state.players;
+    sevenbits_bot = players.filter(function (player) {
+      return player.name == 'sevenbits';
+    })[0];
+    our_cards = sevenbits_bot.hole_cards;
+    score = RANK_SCORE[our_cards[0]['rank']] * RANK_SCORE[our_cards[1]['rank']];
+    if (score > 50) {
+      return 100000;
+    } else {
+      return 0;
+    }
+  } catch (e) {
+    return 0;
+  }
 }
