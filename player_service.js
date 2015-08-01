af1009412f8095a7d4b1a99ea5f33e47f18d9750
Denis Nelubin <dnelubin@gmail.com>
2015-08-01 14:37:01 +0600

    Added service methods to get stat files.

--- player_service.js
+++ player_service.js
@@ -1,15 +1,40 @@
 var player = require('./player');
 var express = require('express');
+var fs = require('fs');
+var path = require('path');
+
 var app = express();
 
 app.use(express.json());
 app.use(express.urlencoded());
 
-app.get('/', function(req, res){
-  res.send(200, 'OK')
+var SUFFIX = '.game_state.json';
+
+app.get('/', function(req, res) {
+  fs.readdir('.', function (err, files) {
+    if (!err) {
+      var links = '';
+      files.filter(function (name) { return name.indexOf(SUFFIX, name.length - SUFFIX.length) !== -1; }).
+          forEach(function (name) { links += '<a href="' + name + '">' + name + '</a><br/>'; });
+      res.send(200, '<html><body>' + links + '</body></html>')
+    } else {
+      res.send(200, 'OK')
+    }
+  });
 });
 
-app.post('/', function(req, res){
+app.get(/\.game_state\.json$/, function(req, res) {
+  fs.readFile(path.basename(req.path), function (err, data) {
+    if (err) {
+      res.send(500, err);
+    } else {
+      res.set('Content-Type', 'text/json');
+      res.send(200, data);
+    }
+  });
+});
+
+app.post('/', function(req, res) {
   if(req.body.action == 'bet_request') {
     res.send(200, player.bet_request(JSON.parse(req.body.game_state)).toString());
   } else if(req.body.action == 'showdown') {
